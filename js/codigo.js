467caa4d1c6be611acec2ceed6776f2fd30da290
wrgalvao0 <warleygalvao61@gmail.com>
2025-04-25 14:21:05 -0300

    projeto funcionando

---
+++ js/codigo.js
@@ -0,0 +1,15 @@
+let botao = document.getElementById('botao')
+botao.addEventListener('click', () => {
+    botao.textContent = 'Gerando...'
+    let container = document.getElementById('container-principal')
+    let existeImg = container.querySelector('img')
+    if(existeImg){
+        container.removeChild(existeImg)
+    }
+    let url = document.getElementById('url')
+    console.log(url.value)
+    let img = document.createElement('img')
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url.value}}`
+    console.log(img)
+    container.appendChild(img)
+})
